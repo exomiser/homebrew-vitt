03f8a7e4adc08283def5b476b39ec3607d2ec809
Jules Jacobsen <j.jacobsen@qmul.ac.uk>
2026-01-16 16:05:02 +0000

    Add v0.1.0 sha256

--- Formula/vitt.rb
+++ Formula/vitt.rb
@@ -2,7 +2,7 @@ class Vitt < Formula
   desc "A moderately small and fast variant annotation utility"
   homepage "https://github.com/exomiser/vitt"
   url "https://github.com/exomiser/vitt/releases/download/v0.1.0/vitt-cli-0.1.0.jar"
-  sha256 "..." # Get with: shasum -a 256 vitt-cli-0.1.0.jar
+  sha256 "a53f376574c12a676e890c6b3f1d270cb7b97146e2fcbf73fe3fbc5d033338a4" # Get with: shasum -a 256 vitt-cli-0.1.0.jar
   version "0.1.0"
 
   depends_on "openjdk"
