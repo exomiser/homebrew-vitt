3d4798682e7f1e3b977b9b68d85c92001fc4cdef
Jules Jacobsen <j.jacobsen@qmul.ac.uk>
2026-01-19 10:04:06 +0000

    Revert pinning java version and add java option to allow `sun.misc.Unsafe` to be called by protobuf

--- Formula/vitt.rb
+++ Formula/vitt.rb
@@ -5,11 +5,11 @@ class Vitt < Formula
   url "https://repo1.maven.org/maven2/org/monarchinitiative/vitt/vitt-cli/#{version}/vitt-cli-#{version}.jar"
   sha256 "ee25886bc831b79d1b30922aaf41fbcda911ef7d712b10f892e37fca5f8a5fbd" # Get with: shasum -a 256 vitt-cli-#{version}.jar
 
-  depends_on "openjdk@21"
+  depends_on "openjdk"
 
   def install
     libexec.install "vitt-cli-#{version}.jar"
-    bin.write_jar_script libexec/"vitt-cli-#{version}.jar", "vitt"
+    bin.write_jar_script libexec/"vitt-cli-#{version}.jar", "vitt", "--sun-misc-unsafe-memory-access=allow"
   end
 
   test do
